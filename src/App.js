9e41241f05db977f473af9164043a11bddff8364
Elliott <elliott@localhost.localdomain>
2020-05-30 08:35:03 -0400

    IMPELMENT: some intial layout/styling

--- src/App.js
+++ src/App.js
@@ -1,187 +1,36 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { withStyles } from '@material-ui/core/styles';
-import TableCell from '@material-ui/core/TableCell';
-import Paper from '@material-ui/core/Paper';
-import { AutoSizer, Column, Table } from 'react-virtualized';
+import {sample} from 'lodash';
+import './App.css'
 
-const styles = (theme) => ({
-  flexContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    boxSizing: 'border-box',
-  },
-  table: {
-    // temporary right-to-left patch, waiting for
-    // https://github.com/bvaughn/react-virtualized/issues/454
-    '& .ReactVirtualized__Table__headerRow': {
-      flip: false,
-      paddingRight: theme.direction === 'rtl' ? '0 !important' : undefined,
-    },
-  },
-  tableRow: {
-    cursor: 'pointer',
-  },
-  tableRowHover: {
-    '&:hover': {
-      backgroundColor: theme.palette.grey[200],
-    },
-  },
-  tableCell: {
-    flex: 1,
-  },
-  noClick: {
-    cursor: 'initial',
-  },
-});
-
-class MuiVirtualizedTable extends React.PureComponent {
-  static defaultProps = {
-    headerHeight: 48,
-    rowHeight: 48,
-  };
-
-  getRowClassName = ({ index }) => {
-    const { classes, onRowClick } = this.props;
-
-    return clsx(classes.tableRow, classes.flexContainer, {
-      [classes.tableRowHover]: index !== -1 && onRowClick != null,
-    });
-  };
-
-  cellRenderer = ({ cellData, columnIndex }) => {
-    const { columns, classes, rowHeight, onRowClick } = this.props;
-    return (
-      <TableCell
-        component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, {
-          [classes.noClick]: onRowClick == null,
-        })}
-        variant="body"
-        style={{ height: rowHeight }}
-        align={(columnIndex != null && columns[columnIndex].numeric) || false ? 'right' : 'left'}
-      >
-        {cellData}
-      </TableCell>
-    );
-  };
-
-  headerRenderer = ({ label, columnIndex }) => {
-    const { headerHeight, columns, classes } = this.props;
-
-    return (
-      <TableCell
-        component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, classes.noClick)}
-        variant="head"
-        style={{ height: headerHeight }}
-        align={columns[columnIndex].numeric || false ? 'right' : 'left'}
-      >
-        <span>{label}</span>
-      </TableCell>
-    );
-  };
-
-  render() {
-    const { classes, columns, rowHeight, headerHeight, ...tableProps } = this.props;
-    return (
-      <AutoSizer>
-        {({ height, width }) => (
-          <Table
-            height={height}
-            width={width}
-            rowHeight={rowHeight}
-            gridStyle={{
-              direction: 'inherit',
-            }}
-            headerHeight={headerHeight}
-            className={classes.table}
-            {...tableProps}
-            rowClassName={this.getRowClassName}
-          >
-            {columns.map(({ dataKey, ...other }, index) => {
-              return (
-                <Column
-                  key={dataKey}
-                  headerRenderer={(headerProps) =>
-                    this.headerRenderer({
-                      ...headerProps,
-                      columnIndex: index,
-                    })
-                  }
-                  className={classes.flexContainer}
-                  cellRenderer={this.cellRenderer}
-                  dataKey={dataKey}
-                  {...other}
-                />
-              );
-            })}
-          </Table>
-        )}
-      </AutoSizer>
-    );
-  }
-}
-
-MuiVirtualizedTable.propTypes = {
-  classes: PropTypes.object.isRequired,
-  columns: PropTypes.arrayOf(
-    PropTypes.shape({
-      dataKey: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      numeric: PropTypes.bool,
-      width: PropTypes.number.isRequired,
-    }),
-  ).isRequired,
-  headerHeight: PropTypes.number,
-  onRowClick: PropTypes.func,
-  rowHeight: PropTypes.number,
-};
-
-const VirtualizedTable = withStyles(styles)(MuiVirtualizedTable);
-
-// ---
-
-var bible = require('./t_kjv.json');
 var books = require('./books.json');
-var rows = bible.resultset.row.map((v) => {
+var bible = require('./t_kjv.json').resultset.row.map((v) => {
   return {bookNum: v.field[1], book: books[v.field[1]-1], chapter: v.field[2], verse:v.field[3], text:v.field[4]};
   }
 );
 
-export default function ReactVirtualizedTable() {
+class Verse extends React.PureComponent{
+  render(){
+    const {verse, isContext} = this.props;
+    return (
+      <span className={isContext?'Context': null}><sup>{verse.verse}</sup>{verse.text}</span>
+    )
+  }
+}
+
+export default function App() {
+  const rnd = Math.floor(Math.random() * bible.length);
+  const beforeVerse = bible[rnd-1];
+  const verse = bible[rnd]
+  const afterVerse = bible[rnd+1]
+
   return (
-    <Paper style={{ height: 950, width: '100%' }}>
-      <VirtualizedTable
-        rowCount={rows.length}
-        rowGetter={({ index }) => rows[index]}
-        columns={[
-          {
-            label: 'Book #',
-            dataKey: 'bookNum',
-            numeric: true,
-          },
-          {
-            label: 'Book',
-            dataKey: 'book',
-          },
-          {
-            label: 'Chapter',
-            dataKey: 'chapter',
-            numeric: true,
-          },
-          {
-            label: 'Verse',
-            dataKey: 'verse',
-            numeric: true,
-          },
-          {
-            label: 'Text',
-            dataKey: 'text',
-          },
-        ]}
-      />
-    </Paper>
+    <div className="App">
+      <input className="Search" name="search" type="text" value="Search..."/>
+      <h1>{verse.book} {verse.chapter}:{verse.verse}</h1>
+      <Verse verse={beforeVerse} isContext={true}/>
+      <Verse verse={verse}/>
+      <Verse verse={afterVerse} isContext={true}/>
+    </div>
   );
 }
+
