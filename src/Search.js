a8d0e96df1415de30594dbdc99ceb23105586e31
Elliott Sperlazza <elliott@localhost.localdomain>
2020-06-03 22:08:56 -0400

    WIP: more reformatting of search results; beginning to break up components; added prettier formatter

---
+++ src/Search.js
@@ -0,0 +1,125 @@
+import React from "react";
+import { filter, take } from "lodash";
+import "./App.css";
+import Mousetrap from "mousetrap";
+import ReactVirtualizedTable from "./ReactVirtualizedTable.js";
+
+class SearchResults extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shouldDisplay: false
+    };
+  }
+
+  render() {
+    const { results, keywords } = this.props;
+
+    return (
+      <div className="SearchResults">
+        <ul>
+          {results.map((v, i) => (
+            <li key={i} versenum={v.index} title={v.text}>
+              {v.book} {v.chapter}:{v.verse} {v.text}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Search extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: "",
+      results: [],
+      searchFocused: false
+    };
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.searchRef = React.createRef();
+  }
+
+  handleSearchChange(event) {
+    this.setState({ searchText: event.target.value });
+    this.doSearch(event.target.value);
+  }
+
+  doSearch(text) {
+    const { bible } = this.props;
+    const searches = [...text.matchAll(/((?:\d\s*)?\w+)\s+(\d+):(\d+(?:-\d+)?)\s*/g)];
+    var verses = [];
+    if (searches.length > 0) {
+      verses = filter(bible, {
+        book: searches[0][1],
+        chapter: parseInt(searches[0][2]),
+        verse: parseInt(searches[0][3])
+      });
+    } else if (text.length > 0) {
+      verses = filter(bible, (v) => v.text.toLowerCase().includes(text.toLowerCase()));
+    }
+    this.setState({ results: verses });
+  }
+
+  componentDidMount() {
+    Mousetrap.bind("shift shift", () => this.searchRef.current.select(), "keyup");
+    Mousetrap.bind("esc", () => this.searchRef.current.blur(), "keyup");
+  }
+
+  componentWillUnmount() {
+    Mousetrap.unbind("shift shift");
+    Mousetrap.unbind("esc");
+  }
+
+  render() {
+    const { searchText, results } = this.state;
+    const searchId = "searchId";
+    const rowConfig = [
+      {
+        //   width: 100%,
+        label: "Book",
+        dataKey: "book",
+        numeric: false
+      },
+      {
+        //   width: 100%,
+        label: "Chapter",
+        dataKey: "chapter",
+        numeric: false
+      },
+      {
+        //   width: 100%,
+        label: "Verse",
+        dataKey: "verse",
+        numeric: false
+      },
+      {
+        //   width: 100%,
+        label: "Text",
+        dataKey: "text",
+        numeric: false
+      }
+    ];
+
+    return (
+      <div>
+        <input
+          id={searchId}
+          className="Search"
+          name="search"
+          type="text"
+          placeholder="Search..."
+          value={searchText}
+          onChange={this.handleSearchChange}
+          ref={this.searchRef}
+        />
+        {searchId === document.activeElement.id && results.length > 0 ? (
+          <ReactVirtualizedTable rows={results} rowConfig={rowConfig} />
+        ) : null}
+      </div>
+    );
+  }
+}
+
+export default Search;
